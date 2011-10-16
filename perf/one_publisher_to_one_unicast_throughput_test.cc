b6f42ff248e1a704875fd0a07e63ecf7ea06bffb
François Saint-Jacques <fsaintjacques@gmail.com>
2011-10-16 14:28:34 -0400

    bugfix: clean way to stop a processor is by calling EventProcessor.Halt()

--- perf/one_publisher_to_one_unicast_throughput_test.cc
+++ perf/one_publisher_to_one_unicast_throughput_test.cc
@@ -11,46 +11,16 @@
 #include <disruptor/event_processor.h>
 #include <disruptor/exception_handler.h>
 
-struct DummyEvent {
-    DummyEvent() : count(0) {};
-    int count;
-};
+#include "../test/support/stub_event.h"
 
 using namespace disruptor;
-
-class DummyBatchHandler : public EventHandlerInterface<DummyEvent> {
- public:
-    virtual void OnEvent(DummyEvent* event, const int64_t& sequence, bool end_of_batch) {
-        if (event)
-            event->count += 1;
-    };
-
-    virtual void OnStart() {}
-    virtual void OnShutdown() {}
-};
-
-class DummyEventFactory : public EventFactoryInterface<DummyEvent> {
- public:
-    virtual DummyEvent* NewInstance(const int& size) const {
-        return new DummyEvent[size];
-    };
-};
-
-class DummyEventTranslator : public EventTranslatorInterface<DummyEvent> {
- public:
-    virtual DummyEvent* TranslateTo(DummyEvent* event, const int64_t& sequence) {
-        event->count=sequence;
-        return event;
-    };
-
-};
 
 int main(int arc, char** argv) {
     int buffer_size = 1024 * 8;
     long iterations = 1000L * 1000L * 300;
 
-    DummyEventFactory dummy_factory;
-    RingBuffer<DummyEvent> ring_buffer(&dummy_factory,
+    test::StubEventFactory stub_factory;
+    RingBuffer<test::StubEvent> ring_buffer(&stub_factory,
                                        buffer_size,
                                        kSingleThreadedStrategy,
                                        kBusySpinStrategy);
@@ -59,21 +29,21 @@ int main(int arc, char** argv) {
     std::unique_ptr<ProcessingSequenceBarrier> barrier(
         ring_buffer.NewBarrier(sequence_to_track));
 
-    DummyBatchHandler dummy_handler;
-    IgnoreExceptionHandler<DummyEvent> dummy_exception_handler;
-    BatchEventProcessor<DummyEvent> processor(&ring_buffer,
+    test::StubBatchHandler stub_handler;
+    IgnoreExceptionHandler<test::StubEvent> stub_exception_handler;
+    BatchEventProcessor<test::StubEvent> processor(&ring_buffer,
                                               (SequenceBarrierInterface*) barrier.get(),
-                                              &dummy_handler,
-                                              &dummy_exception_handler);
+                                              &stub_handler,
+                                              &stub_exception_handler);
 
-    std::thread consumer(std::ref<BatchEventProcessor<DummyEvent>>(processor));
+    std::thread consumer(std::ref<BatchEventProcessor<test::StubEvent>>(processor));
 
     struct timeval start_time, end_time;
 
     gettimeofday(&start_time, NULL);
 
-    std::unique_ptr<DummyEventTranslator> translator(new DummyEventTranslator);
-    EventPublisher<DummyEvent> publisher(&ring_buffer);
+    std::unique_ptr<test::StubEventTranslator> translator(new test::StubEventTranslator);
+    EventPublisher<test::StubEvent> publisher(&ring_buffer);
     for (long i=0; i<iterations; i++) {
         publisher.PublishEvent(translator.get());
     }
@@ -92,7 +62,7 @@ int main(int arc, char** argv) {
     std::cout << (iterations * 1.0) / (end - start)
               << " ops/secs" << std::endl;
 
-    barrier->Alert();
+    processor.Halt();
     consumer.join();
 
     return EXIT_SUCCESS;
