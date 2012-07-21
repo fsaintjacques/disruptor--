b6d2d0ed5fabfc575fbe8d2f762b052629f3d0d4
Jonathan Wakely <redi@kayari.org>
2012-07-21 13:21:30 +0100

    Use deleted functions to disallow copying

--- disruptor/utils.h
+++ disruptor/utils.h
@@ -30,7 +30,7 @@
 // A macro to disallow the copy constructor and operator= functions
 // This should be used in the private: declarations for a class
 #define DISALLOW_COPY_AND_ASSIGN(TypeName) \
-  TypeName(const TypeName&);               \
-  void operator=(const TypeName&)
+  TypeName(const TypeName&) = delete;      \
+  void operator=(const TypeName&) = delete
 
 #endif // DISRUPTOR_UTILS_H_ NOLINT
