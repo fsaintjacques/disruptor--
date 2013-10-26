18744b9b849815af3bedac5f50e02d1bcbaedcaa
chenkai036 <chenkai036@gmail.com>
2013-10-26 21:21:42 +0800

    Add multiple consumers unicast perf test
    Misc changes - remove CMake build script

--- perf/premake4.lua
+++ perf/premake4.lua
@@ -1,10 +1,15 @@
 project "perf_1p_1ep_unicast"
   kind "ConsoleApp"
   language "C++"
-  files { "disruptor/*.h", "one_publisher_to_one_unicast_throughput_test.cc" }
+  files { "one_publisher_to_one_unicast_throughput_test.cc" }
 
 project "perf_1p_3ep_pipeline"
   kind "ConsoleApp"
   language "C++"
-  files { "disruptor/*.h", "one_publisher_to_three_pipeline_throughput_test.cc" }
+  files { "one_publisher_to_three_pipeline_throughput_test.cc" }
+
+project "perf-1p3c"
+  kind "ConsoleApp"
+  language "C++"
+  files { "1p3c.cpp" }
 
